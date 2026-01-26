e882e889589953c2e17532aa88014b934c529802
Geethapriya <priyaes090@gmail.com>
2026-01-26 15:48:08 +0530

    Basic Structure Added

---
+++ academics.php
@@ -0,0 +1,9 @@
+<?php
+// Include templates
+require_once __DIR__ . '/templates/header.php';
+require_once __DIR__ . '/templates/navigation.php';
+?>
+<!-- Main Content-->
+ <?php
+require_once __DIR__ . '/templates/footer.php';
+?>
---
+++ achievements.php
@@ -0,0 +1,10 @@
+<?php
+// Include templates
+require_once __DIR__ . '/templates/header.php';
+require_once __DIR__ . '/templates/navigation.php';
+?>
+<!--Main content-->
+<!--footer>
+<?php
+require_once __DIR__ . '/templates/footer.php';
+?>
